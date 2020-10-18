54b6ce9f727610eb0eea46688cc0ebc7f62c0180
Rafael Paiva <r.rafapaiva@gmail.com>
2020-10-18 22:50:07 +1000

    database config

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findAll(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment => {
+      return isEqual(date, appointment.date);
+    });
+
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentRepository;
